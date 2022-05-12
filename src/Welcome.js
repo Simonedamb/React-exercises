1bf458d27982a9a5419c36141d48b1d0293671ea
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-12 09:49:10 +0200

    conditional-rendering-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,7 +8,7 @@ class Welcome extends React.Component {
         <p>
           Welcome <strong>{this.props.name}</strong> !
         </p>
-        <Age age={25} />
+        <Age age={15} />
       </div>
     );
   }
