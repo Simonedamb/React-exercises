b2381d911be2b9993990a8ed44e7f57198c90637
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-18 19:06:22 +0200

    useEffect-02

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ClickCounter from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { InteractiveWelcome } from "./InteractiveWelcome";
@@ -6,68 +6,50 @@ import { Login } from "./Login";
 import { TodoList } from "./TodoList";
 import { UcontrolledLogin } from "./UncontrolledLogin";
 import Counter from "./Counter";
-import { Container } from "./Container";
-import { DisplayLanguage } from "./DisplayLanguage";
 import { Language } from "./Language";
 import Sum from "./Sum";
 
-export class App extends React.Component {
-  state = {
-    language: "en",
-  };
+export function App() {
+  const [showCounter, setShowCounter] = useState(true);
 
-  handleLanguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <select
-          value={this.state.language}
-          onChange={this.handleLanguageChange}
-        >
-          <option value="en">ENGLISH</option>
-          <option value="it">ITALIANO</option>
-        </select>
-        <Container title={"WELCOME REACT"}>
-          <DisplayLanguage.Provider value={this.state.language}>
-            <Language />
-            <ClickCounter />
-            <Counter />
-            <InteractiveWelcome />
-            <UcontrolledLogin />
-            <Sum />
-            <TodoList
-              render={(arr, addTodo, reset, removeTodo) => {
-                return (
-                  <div>
-                    <form onSubmit={addTodo}>
-                      <input name="list" type="text" />
-                      <button type="submit">Submit</button>
-                      <button onClick={reset}>reset</button>
-                    </form>
-                    <ul>
-                      {arr.map((item, index) => {
-                        return (
-                          <div>
-                            <li>{item}</li>
-                            <button onClick={() => removeTodo(index)}>X</button>
-                          </div>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                );
-              }}
-            />
-            <Login />
-            <ClickTracker />
-          </DisplayLanguage.Provider>
-        </Container>
-      </>
-    );
+  function handleToggleCounter() {
+    setShowCounter((s) => !s);
   }
+
+  return (
+    <>
+      <Language />
+      <ClickCounter />
+      <InteractiveWelcome />
+      <UcontrolledLogin />
+      <Sum />
+      <TodoList
+        render={(arr, addTodo, reset, removeTodo) => {
+          return (
+            <div>
+              <form onSubmit={addTodo}>
+                <input name="list" type="text" />
+                <button type="submit">Submit</button>
+                <button onClick={reset}>reset</button>
+              </form>
+              <ul>
+                {arr.map((item, index) => {
+                  return (
+                    <div>
+                      <li>{item}</li>
+                      <button onClick={() => removeTodo(index)}>X</button>
+                    </div>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        }}
+      />
+      <button onClick={handleToggleCounter}>Toggle Counter</button>
+      {showCounter && <Counter />}
+      <Login />
+      <ClickTracker />
+    </>
+  );
 }
