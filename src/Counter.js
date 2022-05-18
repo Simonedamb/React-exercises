e08e8f8387a597e45f9c463e0812deca30c11d81
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-18 18:19:38 +0200

    useEffect-01

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Counter({ initialValue = 0 }) {
   const [counter, setCounter] = useState(initialValue);
 
-  function handleCounterIncrement() {
+  useEffect(() => {
+    console.log(`counter is ${counter}`);
+  });
+
+  function onCounterChange() {
     setCounter(counter + 1);
   }
 
   return (
     <div>
       <h2>Counter : {counter} </h2>
-      <button onClick={handleCounterIncrement}>Increment</button>
+      <button onClick={onCounterChange}>Increment</button>
     </div>
   );
 }
