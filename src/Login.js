2c4a1a01127ab11f62bf4d401b13cb6f329ed63e
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-19 19:31:44 +0200

    custom-hooks-02

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,43 @@
 import React, { useState } from "react";
 
-export function Login() {
+function useLogin() {
   const [data, setData] = useState({
     username: "",
     password: "",
     remember: false,
   });
 
-  function handleInputChange(event) {
+  function inputChange(event) {
     const { name, type, value, checked } = event.target;
+
     setData({
       [name]: type === "checkbox" ? checked : value,
     });
   }
+  return {
+    data: data,
+    onInputChange: inputChange,
+  };
+}
+
+export const Login = ({ username, password }) => {
+  const { data, onInputChange } = useLogin(username, password);
 
   return (
     <form>
+      <input onChange={onInputChange} value={data.username} name="username" />
       <input
-        onChange={handleInputChange}
-        value={data.username}
-        name="username"
-      />
-      <input
-        onChange={handleInputChange}
+        onChange={onInputChange}
         type="password"
         value={data.password}
         name="password"
       />
       <input
-        onChange={handleInputChange}
+        onChange={onInputChange}
         checked={data.remember}
         type="checkbox"
         name="remember"
       />
     </form>
   );
-}
+};
