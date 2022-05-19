809f9975f3b1abf603e50216deefa36f84e60ba9
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-19 19:49:49 +0200

    custom-hooks-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,13 +1,7 @@
-import { useEffect, useState } from "react";
+import { useGithubUser } from "./useGithubUser";
 
 function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${username}`)
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, [username]);
+  const { data } = useGithubUser(username);
 
   return <div>{data && <h1>{data.name}</h1>}</div>;
 }
