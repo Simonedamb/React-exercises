10c170ea877506c243601d0074987e341ff719d7
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-24 19:05:13 +0200

    react-router-04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GithubUserList } from "./GithubUserList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function App() {
   return (
     <>
       <Router>
+        <div>
+          <Link to="/Welcome">Welcome|</Link>
+          <Link to="/username">Username|</Link>
+          <Link to="/counter">Counter|</Link>
+        </div>
+
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/counter" element={<Counter />} />
