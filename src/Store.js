f009b6130562dd97d0a9d18f5bae87eeffddc8f1
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-05-31 10:49:56 +0200

    redux-02

---
+++ src/Store.js
@@ -0,0 +1,18 @@
+import { createStore } from "redux";
+import {
+  counterReducer,
+  decrementCounter,
+  incrementCounter,
+  resetCounter,
+} from "./CounterReducer";
+
+export const store = createStore(counterReducer);
+
+store.dispatch(incrementCounter(5));
+store.dispatch(decrementCounter(3));
+store.dispatch(decrementCounter(6));
+store.dispatch(resetCounter());
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./App";
+// import { App } from "./App";
+import { store } from "./Store";
+import {
+  incrementCounter,
+  decrementCounter,
+  resetCounter,
+} from "./CounterReducer";
 
-const root = document.querySelector("#root");
+// const root = document.querySelector("#root");
 
-ReactDOM.render(<App />, root);
+// ReactDOM.render(<App />, root);
+
+store.dispatch(incrementCounter(6));
+store.dispatch(decrementCounter(1));
+store.dispatch(decrementCounter(6));
+store.dispatch(resetCounter());
